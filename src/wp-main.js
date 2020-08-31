2d8f0a8d5ffba237de700862143180eb501ead82
alejandro.arroyo.duque.contractor <alejandro.arroyo.duque.contractor@bbva.com>
2020-08-31 23:47:17 +0200

    fix(style): footer

--- src/wp-main.js
+++ src/wp-main.js
@@ -55,13 +55,13 @@ export class WpMain extends LitElement {
       }
 
       footer {
-        color: var(--theme-color-secondary);
+        color: #E0E0E0;
         font-size: .9rem;
         text-align: center;
         padding: .8rem;
       }
       footer a {
-        color: var(--theme-color-secondary);
+        color: #E0E0E0;
       }
       footer p {
         margin: .2rem;
@@ -87,7 +87,7 @@ export class WpMain extends LitElement {
       <footer>
         <p>Toda la propiedad intelectual de esta información pertenece a HBO.</p>
         <p>PETEYPEDIA en español adaptada por @AlexArroyoDuque.</p>
-        <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />Este obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">licencia de Creative Commons Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional</a>.
+        <a rel="license" target="_blank" href="http://creativecommons.org/licenses/by-nc-sa/4.0/"><img alt="Licencia de Creative Commons" style="border-width:0" src="https://i.creativecommons.org/l/by-nc-sa/4.0/88x31.png" /></a><br />Este obra está bajo una <a rel="license" href="http://creativecommons.org/licenses/by-nc-sa/4.0/">licencia de Creative Commons Reconocimiento-NoComercial-CompartirIgual 4.0 Internacional</a>.
       </footer>
 
     `;
